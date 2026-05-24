ec58b3954dbaeda745748eebeb149ce6142cf626
Raees Mughal <raees.mughal.official@gmail.com>
2026-05-24 23:40:29 +0500

    useEffect crypto tracker

---
+++ 08-useEffect-hook/src/components/CryptoTicker.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CryptoTicker = () => {
+  return (
+    <div>
+        <h2>Real-Time Crypto Tracker</h2>
+        
+    </div>
+  )
+}
+
+export default CryptoTicker
